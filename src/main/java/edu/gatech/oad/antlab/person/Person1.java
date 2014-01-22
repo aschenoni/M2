49c0075379e821ddb7047bd48a5d5d22eab62a76
Austin Joyner <abjoyner@gmail.com>
2014-01-22 15:55:43 -0500

    Coded the calc(string) method for Person1.java

--- src/main/java/edu/gatech/oad/antlab/person/Person1.java
+++ src/main/java/edu/gatech/oad/antlab/person/Person1.java
@@ -3,23 +3,24 @@ package edu.gatech.oad.antlab.person;
 /**
  *  A simple class for person 1
  *  returns their name and a
- *  modified string 
- *  
+ *  modified string
+ * 
  *  @author Bob
  *  @version 1.1
  */
 public class Person1 {
-  /** Holds the persons real name */
-  private String name;
-  	/**
+	/** Holds the persons real name */
+	private String name;
+	/**
 	 * The constructor, takes in the persons
 	 * name
 	 * @param pname the person's real name
 	 */
-  public Person1(String pname) {
-    name = pname;
-  }
-  	/**
+	public Person1(String pname) {
+		name = pname;
+	}
+
+	/**
 	 * This method should take the string
 	 * input and return its characters rotated
 	 * 2 positions.
@@ -30,20 +31,36 @@ public class Person1 {
 	 * @return the modified string
 	 */
 	private String calc(String input) {
-	  //Person 1 put your implementation here
-	  return null;
+		//Person 1 put your implementation here
+
+		String temp = new String();
+		int shift = 2;
+
+		for(int i =0; i < input.length(); i++)
+		{
+			if( (i + shift) >= input.length() )
+			{
+				int position = i + shift - input.length();
+				temp = temp + input.substring(position, (position + 1) );
+			}
+			else
+			{
+
+				temp = temp + input.substring( (i+shift) , (i+shift + 1) );
+			}
+		}
+		return temp;
 	}
-	
+
 	/**
 	 * Return a string rep of this object
 	 * that varies with an input string
 	 *
 	 * @param input the varying string
-	 * @return the string representing the 
+	 * @return the string representing the
 	 *         object
 	 */
 	public String toString(String input) {
-	  return name + calc(input);
+		return name + calc(input);
 	}
-
 }
